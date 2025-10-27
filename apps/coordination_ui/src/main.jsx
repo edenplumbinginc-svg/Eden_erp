c86c528eaba06fda07dcaf9eec34a35e60e09dc4
Eden ERP Auto-Sync <eden-erp-bot@edenplumbing.com>
2025-10-27 00:09:16 +0000

    Auto-sync from Replit 2025-10-27 00:09:16

--- apps/coordination_ui/src/main.jsx
+++ apps/coordination_ui/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import * as Sentry from '@sentry/react'
+import '@fontsource-variable/inter'
 import App from './App.jsx'
 import './index.css'
 
